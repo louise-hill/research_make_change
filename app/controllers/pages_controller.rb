a4a878262c6bbc4126145f8108022f34db5d6e63
louise hill <louisehilldesigner@gmail.com>
2022-08-06 13:48:49 +0100

    pages set up

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,22 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def work
+  end
+
+  def transformation
+  end
+
+  def ai
+  end
+
+  def brandstrategy
+  end
+
+  def innovation
+  end
+
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  root to: "pages#home"
+  get 'errors/not_found'
+  get 'errors/internal_server_error'
+  get 'pages/home'
+  get 'pages/work'
+  get 'pages/transformation'
+  get 'pages/ai'
+  get 'pages/brandstrategy'
+  get 'pages/about'
+  get 'pages/productinnovation'
+  match "/404", to: "errors#not_found", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
